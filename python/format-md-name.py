0f36ecf2a72b735fe34623f4c567de43f4bf703d
samzong.lu <samzong.lu@gmail.com>
2023-07-14 14:07:12 +0800

    update markdown file name to pinyin
    Signed-off-by: samzong.lu <samzong.lu@gmail.com>

--- python/format-md-name.py
+++ python/format-md-name.py
@@ -11,20 +11,15 @@ for file_name in os.listdir(current_dir):
     if file_name.endswith(".md"):
         # 提取文件名和扩展名
         name, ext = os.path.splitext(file_name)
-        print(name, ext)
 
         name = name.replace(' ', '-').replace('_', '-')
-        print(lazy_pinyin(name))
-        print(name)
-
-        new_name = '-'.join(lazy_pinyin(name))
-        new_name = re.sub(r'-{2,}', '-', new_name)
-
-        print(new_name)
 
         # 将中文转换为拼音
-        # new_name = ''.join(pinyin(name, style=Style.NORMAL))
-
+        new_name = '-'.join(lazy_pinyin(name))
+        
+        # 去除多余的连字符
+        new_name = re.sub(r'-{2,}', '-', new_name).lower().strip()
+        
         # 构建新的文件路径和文件名
         new_path = os.path.join(current_dir, new_name + ext)
 
